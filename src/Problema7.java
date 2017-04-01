64c9f8d39827f48b0742150f0d3697cb42a20bb5
Nicu Osan <nicuosan@gmail.com>
2017-04-01 17:35:12 +0300

    t7 refacut

--- src/Problema7.java
+++ src/Problema7.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 /**
  * Created by Nicu Osan on 30.03.2017.
  */
@@ -6,25 +8,25 @@ public class Problema7 {
     /*7. Se da un sir de numere. Sa se afiseze perechile de numere pentru care  primul numar este mai mare decat al doilea. */
 
     public static void main(String[] args) {
-        int[] a = {9, 3, 1, 6, 4, 8, 15, 12, 13, 23};
-        int c = a[0];
-        int b = a[1];
-        do {
+        int[] array = {8, 3, 1, 6, 4, 8, 15, 12, 13, 23};
 
+        int index = 0;
+        int first = array[0];
+        int second = array[1];
 
-        boolean cond = (a[c] > a[b]);
-        if (cond) {
-            System.out.print(c + "," + b);
-            c = a[0 + 1];
-            b = a[1 + 1];
+        while (index < array.length - 2) {
+
+            if (first > second) {
+                System.out.println(first + "," + second + " ");
+            }
+            index += 1;
+            first = array[index];
+            second = array[index + 1];
         }
-        else {
-            continue;
 
-        }
+
     }
+}
 
-        while (a.length ==9);
-}}
 
 
